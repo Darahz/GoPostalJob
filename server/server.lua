43516cc390d81870f7a815be5644ed59cc7bfb16
andre <darahzmail@gmail.com>
2022-09-23 12:52:44 +0200

    Basic payment function

--- server/server.lua
+++ server/server.lua
@@ -1,10 +1,10 @@
 --Paycheck something
 local QBCore = exports['qb-core']:GetCoreObject()
 
-RegisterNetEvent('prdx-gopostal:server:server:PayShift', function()
+RegisterNetEvent('prdx-gopostal:server:server:PayShift', function(dist)
     local src = source
     local Player = QBCore.Functions.GetPlayer(src)
     local CitizenId = Player.PlayerData.citizenid
-    Player.Functions.AddMoney("bank", 1000 , 'garbage-payslip')
+    Player.Functions.AddMoney("bank", dist , 'garbage-payslip')
     print("payed?")
 end)
