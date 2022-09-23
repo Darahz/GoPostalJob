59b31bf14d90805bf728e8ccce2a8c2e3b2419e7
andre <darahzmail@gmail.com>
2022-09-23 11:40:09 +0200

    Quick payment boi

--- server/server.lua
+++ server/server.lua
@@ -1,1 +1,10 @@
 --Paycheck something
+local QBCore = exports['qb-core']:GetCoreObject()
+
+RegisterNetEvent('prdx-gopostal:server:server:PayShift', function()
+    local src = source
+    local Player = QBCore.Functions.GetPlayer(src)
+    local CitizenId = Player.PlayerData.citizenid
+    Player.Functions.AddMoney("bank", 1000 , 'garbage-payslip')
+    print("payed?")
+end)
