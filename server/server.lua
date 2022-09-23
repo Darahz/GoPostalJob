a574f377972ebfe98320aba9c32cc710c86e8821
andre <darahzmail@gmail.com>
2022-09-23 12:56:39 +0200

    name Changes

--- server/server.lua
+++ server/server.lua
@@ -5,6 +5,6 @@ RegisterNetEvent('prdx-gopostal:server:server:PayShift', function(dist)
     local src = source
     local Player = QBCore.Functions.GetPlayer(src)
     local CitizenId = Player.PlayerData.citizenid
-    Player.Functions.AddMoney("bank", dist , 'garbage-payslip')
+    Player.Functions.AddMoney("bank", dist , 'gopostal-payslip')
     print("payed?")
 end)
